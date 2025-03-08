2b142d6df38558e203b85171e0c6ab6c55fccadd
Hao Dong <hey@hao.dev>
2025-03-08 15:25:53 +0000

    chore: add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import { VitePWA } from "vite-plugin-pwa";
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-    registerType: "autoUpdate",
-  }),],
-})
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'autoUpdate',
+		}),
+		tailwindcss(),
+	],
+});
